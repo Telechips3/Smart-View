b66d411c108214b17a9dc62613e57a6d451bdeb7
ssamKim0908 <mmmek0401@gmail.com>
2026-01-05 11:09:17 +0900

    modify gitignore

--- Linux/RPI-M/app/source/main_controller.cpp
+++ Linux/RPI-M/app/source/main_controller.cpp
@@ -1,7 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <arpa/inet.h>
+#include <nlohmann/json.hpp>
+#include <opencv2/opencv.hpp>
+#include "ydlidar_sdk.h"
 #include "common.h"
+
+using json = nlohmann::json;
+using namespace std;
+using namespace cv;
 
 int main()
 {
-        printf("hello main_controller\n");
+
     return 0;
 }
